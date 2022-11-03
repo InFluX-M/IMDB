1c2a7b0f2c10733e3275f72eb5baff0c72b5b910
Zahra <zahraamasoumi@gmail.com>
2022-11-03 15:38:58 +0330

    Add ExceptionController and EntityNotFound Exception

---
+++ IMDB/src/main/java/com/example/imdb/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.imdb.exception;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String entityName, String id){
+        super(entityName + " with id " + id + " not found");
+    }
+}
+

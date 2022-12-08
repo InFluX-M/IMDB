a9f662dda601c9f628416f641102f0a5d1e2130e
SheidaAbedpour <sheida.abedpour2002@gmail.com>
2022-12-08 17:18:42 +0330

    toolbar added

--- andriod-imdb/app/src/main/java/com/example/imdbproj/MainActivity.kt
+++ andriod-imdb/app/src/main/java/com/example/imdbproj/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.imdbproj
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout,toolbar,
+                                           R.string.open,R.string.close)
+        toggle.syncState()
+
+
+
     }
+
 }
